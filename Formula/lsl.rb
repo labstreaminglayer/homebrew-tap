6451bf4cdc2fa17915397681d2db0b4192a0fc4b
Chadwick Boulay <chadwick.boulay@gmail.com>
2021-06-11 14:08:52 -0400

    Add dumb test and cleanup text.

--- Formula/lsl.rb
+++ Formula/lsl.rb
@@ -4,16 +4,18 @@ class Lsl < Formula
   homepage "https://labstreaminglayer.readthedocs.io/"
   url "https://github.com/sccn/liblsl", using: :git
   version "1.14.1b9"
-  sha256 "4de0d9726f00430b0f7bda38c44fb40d09461e9aec38fb17b00640f996a2f784"  # openssl dgst -sha256 <file>
+  sha256 "4de0d9726f00430b0f7bda38c44fb40d09461e9aec38fb17b00640f996a2f784"
   license "MIT"
   revision 0
+  # NOTE: use `openssl dgst -sha256 <file>` to get sha256
 
   head "https://github.com/sccn/liblsl.git"
 
   # To make a bottle:
   # 1 - `brew install --build-bottle lsl`
-  # 2 - `brew bottle --root-url="https://github.com/labstreaminglayer/homebrew-tap/releases/download/v1.14" --no-rebuild lsl`
-  # 3 - Upload produced bottle to GH release page above. If there's a double-dash in the filename then you have to remove that first.
+  # 2 - `brew bottle --root-url="https://github.com/labstreaminglayer/homebrew-tap/releases/download/v1.14" lsl`
+  # 3 - Upload produced bottle to GH release page above. If there's a double-dash in the filename
+  #     then you have to remove that first.
   # 4 - Copy the printed information into this block. git add and push this updated formula.
   bottle do
     root_url "https://github.com/labstreaminglayer/homebrew-tap/releases/download/v1.14"
@@ -28,6 +30,6 @@ class Lsl < Formula
   end
 
   test do
-    system "false"
+    system bin/"lslver"
   end
 end
