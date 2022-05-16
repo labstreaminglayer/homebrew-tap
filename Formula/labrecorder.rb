3ebcbded8b2ee6bfe5a614ba195205c496a27d63
Chadwick Boulay <chadwick.boulay@gmail.com>
2022-05-16 01:22:21 -0400

    Bump version after fixup on LabRecorder cmake

--- Formula/labrecorder.rb
+++ Formula/labrecorder.rb
@@ -2,10 +2,10 @@ class Labrecorder < Formula
   desc "Application for streaming one or more LSL streams to disk in XDF file format"
   homepage "https://github.com/labstreaminglayer/App-LabRecorder"
   url "https://github.com/labstreaminglayer/App-LabRecorder", using: :git
-  version "1.16.1"
-  sha256 "f4b4f5eaf0623e3366c54af927f52274d24607dc111abff7911b3c83c4366ec9"
+  version "1.16.2"
+  sha256 "08fa89de28e65e20f8094ee42292f27a17d8fb8f97c4560e65fa110d406b1f00"
   license "MIT"
-  revision 8
+  revision 9
 
   head "https://github.com/labstreaminglayer/App-LabRecorder.git"
 
@@ -22,8 +22,6 @@ class Labrecorder < Formula
     # system "echo", ENV['HOMEBREW_FORMULA_PREFIX']
     system "cmake", "-S", ".", "-B", "build", "-DLSL_DEPLOYAPPLIBS=OFF", *std_cmake_args
     system "cmake", "--build", "build", "--target", "install", "--config", "Release", "-j"
-    # Next line required because macdeployqt does not bundle loader_path dependencies: https://bugreports.qt.io/browse/QTBUG-100686
-    system "cp", "-L", "/opt/homebrew/opt/brotli/lib/libbrotlicommon.1.dylib", ENV['HOMEBREW_FORMULA_PREFIX']/LabRecorder/LabRecorder.app/Contents/Frameworks
   end
 
   test do
