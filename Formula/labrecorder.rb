b8acf258f1072146c04efc22765c1b3eb5c23dbd
Chadwick Boulay <chadwick.boulay@gmail.com>
2022-05-15 23:05:27 -0400

    again

--- Formula/labrecorder.rb
+++ Formula/labrecorder.rb
@@ -5,7 +5,7 @@ class Labrecorder < Formula
   version "1.16.1"
   sha256 "f4b4f5eaf0623e3366c54af927f52274d24607dc111abff7911b3c83c4366ec9"
   license "MIT"
-  revision 6
+  revision 7
 
   head "https://github.com/labstreaminglayer/App-LabRecorder.git"
 
@@ -19,10 +19,11 @@ class Labrecorder < Formula
   depends_on "qt"
 
   def install
+    system "echo", ENV['HOMEBREW_FORMULA_PREFIX']
     system "cmake", "-S", ".", "-B", "build", "-DLSL_DEPLOYAPPLIBS=OFF", *std_cmake_args
     system "cmake", "--build", "build", "--target", "install", "--config", "Release", "-j"
     # Next line required because macdeployqt does not bundle loader_path dependencies: https://bugreports.qt.io/browse/QTBUG-100686
-    system "cp", "-L", "/opt/homebrew/opt/brotli/lib/libbrotlicommon.1.dylib", "ENV['HOMEBREW_FORMULA_PREFIX']/LabRecorder/LabRecorder.app/Contents/Frameworks"
+    system "cp", "-L", "/opt/homebrew/opt/brotli/lib/libbrotlicommon.1.dylib", ENV['HOMEBREW_FORMULA_PREFIX']"/LabRecorder/LabRecorder.app/Contents/Frameworks"
   end
 
   test do
