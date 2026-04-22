1dfc026f900d7f2a480aeec449b8a5b7206e4de3
Chadwick Boulay <chadwick.boulay@gmail.com>
2026-04-21 23:00:30 -0400

    Update recipe for liblsl 1.17.7

--- Formula/lsl.rb
+++ Formula/lsl.rb
@@ -1,17 +1,19 @@
 class Lsl < Formula
   desc "Library for multi-modal time-synched data transmission over the network"
   homepage "https://labstreaminglayer.readthedocs.io/"
-  url "https://github.com/sccn/liblsl/archive/refs/tags/v1.17.4.tar.gz"
-  sha256 "43720576b6a568923a68de8f37e894c0e583ff07ed219d3f5121a3a45f009c7b"
+  url "https://github.com/sccn/liblsl/archive/refs/tags/v1.17.7.tar.gz"
+  sha256 "19604fb1b30c753457e8bf1430341d84012834c0cf8d2ca921e8151d31220df9"
   license "MIT"
   head "https://github.com/sccn/liblsl.git", branch: "main"
 
   depends_on "cmake" => :build
+  depends_on "pugixml"
 
   def install
     args = %W[
       -DLSL_FRAMEWORK=ON
       -DCMAKE_INSTALL_FRAMEWORK_DIR=#{frameworks}
+      -DLSL_FETCH_PUGIXML=OFF
     ]
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
     system "cmake", "--build", "build", "--target", "install", "--config", "Release", "-j"
--- README.md
+++ README.md
@@ -9,3 +9,9 @@ Or `brew tap labstreaminglayer/tap` and then `brew install <formula>`.
 ## Documentation
 
 `brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
+
+## Developers
+
+Update the tar.gz URL, then download the file and calculate the sha sum:
+
+`curl -sL https://github.com/sccn/liblsl/archive/refs/tags/v1.17.7.tar.gz | shasum -a 256`
